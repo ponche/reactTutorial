bff512f50db7eb0ee14c258069f4e65374981c57
Rémi <ponche62880@gmail.com>
2019-09-12 18:32:04 +0200

    court-circuiter en cas de gagant ou case déja jouer sur le clic

--- src/index.js
+++ src/index.js
@@ -25,6 +25,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -39,7 +42,7 @@ class Board extends React.Component {
       status = winner + ' a gagné';
     }
     else {
-       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     }
 
